8b71de95f700f0667773ea0ee2a77c1f91564d30
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-05-12 16:10:51 +0700

    Lottery web by react and web3.

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,6 @@
+import web3 from './web3';
+
+const address = '0xb63f0bc353f9cdf2488AA1b1C3d186cCACDbfcd6';
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
